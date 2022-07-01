33528caa87f524fc56137a3d34454201d21ec897
Matthias Hryniszak <padcom@gmail.com>
2022-07-01 23:43:39 +0200

    Initial import

---
+++ host/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <label>Show UI <input type="checkbox" v-model="showUI" /></label>
+    <div v-if="showUI">
+      <HelloWorld msg="message" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {
+    HelloWorld: () => import('library1/HelloWorld'),
+  },
+  data() {
+    return {
+      message: 'Vue.js application',
+      showUI: false,
+    }
+  },
+}
+</script>
+
+<style>
+.h1 {
+  color: red;
+}
+</style>
---
+++ library1/README.md
@@ -0,0 +1,1 @@
+Please be aware that the vue-loader in version 16.x and later only works with Vue 2.x (or I can't make it work with later versions)
